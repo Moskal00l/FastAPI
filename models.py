800a5fa3c297c1e80b05c6d32afa7bfd7dd050c8
Moskal00l <maila2012@bk.ru>
2026-08-03 22:14:13 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,32 +1,14 @@
 from sqlalchemy import Column, Integer, String, Text
+
 from database import Base
 
 
 class RecipeDB(Base):
-
     __tablename__ = "recipes"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True, nullable=False)
-    cooking_time = Column(Integer, nullable=False)
-    ingredients = Column(Text, nullable=False)
-    description = Column(Text, nullable=True)
-    views = Column(Integer, default=0, nullable=False)
-
-    def __init__(self, **kwargs):
-        if 'views' not in kwargs:
-            kwargs['views'] = 0
-        super().__init__(**kwargs)
-
-    def __repr__(self) -> str:
-        return f"<RecipeDB(id={self.id}, name='{self.name}', views={self.views})>"
-
-    def to_dict(self) -> dict:
-        return {
-            "id": self.id,
-            "name": self.name,
-            "cooking_time": self.cooking_time,
-            "ingredients": self.ingredients,
-            "description": self.description,
-            "views": self.views,
-        }
+    name = Column(String, index=True)
+    cooking_time = Column(Integer)
+    ingredients = Column(Text)
+    description = Column(Text)
+    views = Column(Integer, default=0)
